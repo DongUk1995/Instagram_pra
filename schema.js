67c6bbc32322553b2bfb4c9a51a59c84c7f32635
김동욱 <email@example.com>
2024-07-04 10:13:06 +0900

    upload_start

--- schema.js
+++ schema.js
@@ -1,15 +1,9 @@
-import path from "path";
 import { loadFilesSync } from "@graphql-tools/load-files";
 import { mergeTypeDefs, mergeResolvers } from "@graphql-tools/merge";
-import { makeExecutableSchema } from "graphql-tools";
+import { makeExecutableSchema } from "@graphql-tools/schema";
 
-const loadedTypes = loadFilesSync(path.join(__dirname, "./**/*.typeDefs.js"));
-const loadedResolvers = loadFilesSync(
-  path.join(__dirname, "./**/*.resolvers.js")
-);
+const loadedTypes = loadFilesSync(`${__dirname}/**/*.typeDefs.js`);
+const loadedResolvers = loadFilesSync(`${__dirname}/**/*.resolvers.js`);
 
-const typeDefs = mergeTypeDefs(loadedTypes);
-const resolvers = mergeResolvers(loadedResolvers);
-const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-export default schema;
+export const typeDefs = mergeTypeDefs(loadedTypes);
+export const resolvers = mergeResolvers(loadedResolvers);
--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -4,10 +4,13 @@ import { protectedResolver } from "../users.utils";
 
 const resolverFn = async (
   _,
-  { firstName, lastName, username, email, password: newPassword },
+  { firstName, lastName, username, email, password: newPassword, bio, avatar },
   { loggedInUser }
 ) => {
   //const { id } = await jwt.verify(token, process.env.SECRET_KEY); //사용자가 토큰을 입력을 하면 토큰을 분석하여 리턴을 한다. 그 중에 아이디만 필요함. => conext, utils 참고
+  console.log(avatar);
+  const { filename, createReadStream } = await avatar;
+  console.log(filename, createReadStream);
   let uglyPassword = null;
   if (newPassword) {
     uglyPassword = await bcrypt.hash(newPassword, 10);
@@ -19,6 +22,7 @@ const resolverFn = async (
       lastName,
       username,
       email,
+      bio,
       ...(uglyPassword && { password: uglyPassword }), // newPassword가 변경 하고 싶은 패스워드가 있으면 newPassword변경해서 리턴함.
     },
   }); // 사용자가 email만 수정하려고 보내면 나머지 unfind로 표시되고 데이터베이스에서는 그냥 eamil만 수정한다.
