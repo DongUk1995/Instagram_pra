6b9f39a40b4f713ddf38c09296b215ee0fad830a
김동욱 <email@example.com>
2024-07-17 09:53:45 +0900

    message_start

--- users/editProfile/editProfile.resolvers.js
+++ users/editProfile/editProfile.resolvers.js
@@ -1,41 +1,44 @@
-import client from "../../client";
+import GraphQLUpload from "graphql-upload/GraphQLUpload.js";
 import bcrypt from "bcrypt";
+import client from "../../client";
 import { protectedResolver } from "../users.utils";
-import { createWriteStream, WriteStream } from "fs";
+import { uploadToS3 } from "../../shared/shared.utils";
 
-const resolverFn = async (
+const editProfileResolver = async (
   _,
-  { firstName, lastName, username, email, password: newPassword, bio, avatar },
+  { username, email, name, password: newPassword, location, avatar },
   { loggedInUser }
 ) => {
-  //const { id } = await jwt.verify(token, process.env.SECRET_KEY); //사용자가 토큰을 입력을 하면 토큰을 분석하여 리턴을 한다. 그 중에 아이디만 필요함. => conext, utils 참고
   let avatarUrl = null;
   if (avatar) {
-    const { filename, createReadStream } = await avatar;
-    const newFileName = `${loggedInUser.id}-${Date.now()}-${filename}`;
-    const readStream = createReadStream();
-    const writeStream = createWriteStream(
-      process.cwd() + "/uploads/" + newFileName
-    );
-    readStream.pipe(writeStream);
-    avatarUrl = `http://localhost:${process.env.PORT}/static/${newFileName}`;
+    avatarUrl = await uploadToS3(avatar, loggedInUser.id, "avatars");
   }
-  let uglyPassword = null;
+  /* const { filename, createReadStream } = await avatar;
+  const newFileName = `${loggedInUser.id}-${Date.now()}-${filename}`;
+  const readStream = createReadStream();
+  const writeStream = createWriteStream(
+    process.cwd() + "/uploads/" + newFileName
+  );
+  readStream.pipe(writeStream);
+  avatarUrl = `http://localhost:${process.env.PORT}/static/${newFileName}`;
+ */
+  let newCryptPassword = null;
   if (newPassword) {
-    uglyPassword = await bcrypt.hash(newPassword, 10);
+    newCryptPassword = await bcrypt.hash(newPassword, 10);
   }
   const updatedUser = await client.user.update({
-    where: { id: loggedInUser.id },
+    where: {
+      id: loggedInUser.id,
+    },
     data: {
-      firstName,
-      lastName,
       username,
       email,
-      bio,
-      ...(uglyPassword && { password: uglyPassword }), // newPassword가 변경 하고 싶은 패스워드가 있으면 newPassword변경해서 리턴함.
+      name,
+      location,
       ...(avatarUrl && { avatar: avatarUrl }),
+      ...(newCryptPassword && { password: newCryptPassword }),
     },
-  }); // 사용자가 email만 수정하려고 보내면 나머지 unfind로 표시되고 데이터베이스에서는 그냥 eamil만 수정한다.
+  });
   if (updatedUser.id) {
     return {
       ok: true,
@@ -43,16 +46,14 @@ const resolverFn = async (
   } else {
     return {
       ok: false,
-      error: "님 프로필 수정하지 못함요 ~",
+      error: "Could not update profile.",
     };
   }
 };
 
 export default {
+  Upload: GraphQLUpload,
   Mutation: {
-    editProfile: protectedResolver(resolverFn),
+    editProfile: protectedResolver(editProfileResolver),
   },
 };
-
-//editProfile: async ( _, { firstName, lastName, username, email, password: newPassword },context )
-//=> context는 모든 resolvers에서 접근이 가능하다.
--- users/unfollowUser/unfollowUser.typeDefs.js
+++ users/unfollowUser/unfollowUser.typeDefs.js
@@ -1,12 +1,7 @@
 import { gql } from "apollo-server-express";
 
 export default gql`
-  type unfollowUserRest {
-    ok: Boolean!
-    error: String
-  }
-
   type Mutation {
-    unfollowUser(username: String!): unfollowUserRest!
+    unfollowUser(username: String!): MutationResponse!
   }
 `;
